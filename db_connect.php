1e4292f9a811a468b8314017dc930b9667523041
zxc0918727460 <zxc0918727460@gmail.com>
2019-07-22 17:06:39 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?
 	header("Content=Type:Text/html; charset=utf-8");
 	$db_host = "203.64.91.58";
-	$db_account = "stu1103108111";
-	$db_password = "qaz8721705";
+	$db_account = "";
+	$db_password = "";
 	$conn = @mysql_connect($db_host , $db_account,$db_password) or die("連線錯誤");
 	
 	mysql_query("SET NAMES 'utf-8'");
