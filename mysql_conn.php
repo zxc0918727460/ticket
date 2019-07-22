e467eaac4b1dc8534c4f7e140d448f2318898759
zxc0918727460 <zxc0918727460@gmail.com>
2019-07-22 17:06:04 +0800

    Update mysql_conn.php

--- mysql_conn.php
+++ mysql_conn.php
@@ -2,8 +2,8 @@
 	header("Content-Type:text/html; charset=utf-8");
 	//資料庫主機設定
 	$db_host = "203.64.91.58";	
-	$db_account = "stu1103108111";		
-	$db_password = "qaz8721705";
+	$db_account = "";		
+	$db_password = "";
 	//連線伺服器
 	$conn = mysql_connect($db_host, $db_account, $db_password) or die("資料庫連線錯誤");
 	//設定字元集與連線校對
